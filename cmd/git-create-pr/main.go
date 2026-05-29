555741077e5538ec4d80fbf0b48c2cd490723073
Jean-Baptiste Langlois <jean-baptiste.langlois@imtf.com>
2026-05-29 22:13:09 +0200

    feat: add parameters to git-create-pr

--- cmd/git-create-pr/main.go
+++ cmd/git-create-pr/main.go
@@ -3,31 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/Mithweth/git-tools/internal/auth"
 	"github.com/Mithweth/git-tools/internal/domain"
 	"github.com/Mithweth/git-tools/internal/git"
 	"github.com/Mithweth/git-tools/internal/providers"
+	"github.com/spf13/pflag"
 	"os"
 )
 
-func CreatePR() (string, error) {
+func CreatePullRequest(message string) (string, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	var err error
-	baseBranch := os.Getenv("BASE_BRANCH")
-	if baseBranch == "" {
-		_, baseBranch, err = git.GetDefaultBranch()
-		if err != nil {
-			return "", err
-		}
+	_, baseBranch, err := git.GetDefaultBranch()
+	if err != nil {
+		return "", err
 	}
-	headBranch := os.Getenv("BRANCH_NAME")
-	if headBranch == "" {
-		_, headBranch, err = git.GetCurrentBranch()
-		if err != nil {
-			return "", err
-		}
+	_, headBranch, err := git.GetCurrentBranch()
+	if err != nil {
+		return "", err
 	}
-	message := os.Getenv("COMMIT_MESSAGE")
 	if message == "" {
 		message, err = git.GetLastCommitMessage()
 		if err != nil {
@@ -60,8 +55,38 @@ func CreatePR() (string, error) {
 	return url, nil
 }
 
+func Push(force bool) error {
+	repository, err := git.GetRepository()
+	if err != nil {
+		return err
+	}
+	authMethod, err := auth.GetAuth(repository)
+	if err != nil {
+		return err
+	}
+	if err := git.Push(authMethod, force); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-	url, err := CreatePR()
+	var (
+		commitMessage string
+		push          bool
+		force         bool
+	)
+	pflag.StringVarP(&commitMessage, "message", "m", "", "pull request title")
+	pflag.BoolVarP(&push, "push", "p", false, "push before creating pull request")
+	pflag.BoolVarP(&force, "force", "f", false, "use force-push instead push")
+	pflag.Parse()
+	if push {
+		if errPush := Push(force); errPush != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", errPush)
+			os.Exit(1)
+		}
+	}
+	url, err := CreatePullRequest(commitMessage)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
